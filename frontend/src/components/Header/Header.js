72ac4ee829c24b0150a00d22d2581352afbf41f0
Yasin <yasinvural0@gmail.com>
2020-10-28 17:55:50 +0300

    header component is created

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -1,8 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
 import "./Header.css";
 
 const Header = () => {
-  return <div className="header">Header Componen</div>;
+  return (
+    <Link to="/">
+      <div className="header">
+        <div className="appName">AppStore</div>
+        <div className="searchContainer">
+          <input type="text" placeholder="Seach for an application here..." />
+        </div>
+        <div className="loginButton">Login</div>
+      </div>
+    </Link>
+  );
 };
 
 export default Header;
