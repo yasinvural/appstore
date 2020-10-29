cf6bca9a4d05bd8473c8d12aba338b050fa171ea
Yasin <yasinvural0@gmail.com>
2020-10-29 18:13:29 +0300

    loading is added

--- frontend/src/components/AppList/AppList.js
+++ frontend/src/components/AppList/AppList.js
@@ -1,32 +1,49 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import { useAppState } from "../../context/appContext";
 import { baseService } from "../../services/index";
-import { SET_APPLICATIONS } from "../../const/index";
+import { SET_APPLICATIONS, SET_APP_ISLOADING } from "../../const/index";
 import Card from "../Card/Card";
 
 import "./AppList.css";
 
 const AppList = () => {
   const { app, dispatch } = useAppState();
-  const { applications, search } = app;
+  const { applications, search, isLoading } = app;
 
   useEffect(() => {
     async function fetchAppList() {
+      dispatch({
+        type: SET_APP_ISLOADING,
+        payload: true,
+      });
       const res = await baseService.get("/applications");
       dispatch({
         type: SET_APPLICATIONS,
         payload: res.data,
       });
+      dispatch({
+        type: SET_APP_ISLOADING,
+        payload: false,
+      });
     }
+
     fetchAppList();
   }, []);
 
   useEffect(() => {
     async function fetchAppList() {
+      dispatch({
+        type: SET_APP_ISLOADING,
+        payload: true,
+      });
       const res = await baseService.get(`/search?name=${search}`);
       dispatch({
         type: SET_APPLICATIONS,
         payload: res.data,
+      });
+      dispatch({
+        type: SET_APP_ISLOADING,
+        payload: false,
       });
     }
     fetchAppList();
@@ -37,6 +54,7 @@ const AppList = () => {
       {applications.map((app) => (
         <Card key={app._id} app={app} />
       ))}
+      {isLoading && <div className="loader"></div>}
     </div>
   );
 };
--- frontend/src/reducer/appReducer.js
+++ frontend/src/reducer/appReducer.js
@@ -1,7 +1,12 @@
-import { SET_APPLICATIONS, SET_SEARCH } from "../const/index";
+import {
+  SET_APPLICATIONS,
+  SET_SEARCH,
+  SET_APP_ISLOADING,
+} from "../const/index";
 
 const initialState = {
   applications: [],
+  isLoading: false,
   search: "",
 };
 
@@ -11,6 +16,11 @@ const appReducer = (state, action) => {
       return {
         ...state,
         applications: action.payload,
+      };
+    case SET_APP_ISLOADING:
+      return {
+        ...state,
+        isLoading: action.payload,
       };
     case SET_SEARCH:
       return {
